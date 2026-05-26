0911c78a716009e60c96c9e548d04be0561e98b0
restumardiko <mardikorestu@gmail.com>
2026-05-26 12:15:54 +0700

    "move api folder to the adapter"

--- src/mocks/data/tokenStore.js
+++
@@ -1,5 +0,0 @@
-export const tokenStore = {
-  accessToken: null,
-  refreshToken: null,
-  accessExpiredAt: null,
-};
--- src/mocks/utils/token.js
+++
@@ -1,3 +0,0 @@
-export const generateToken = (type = "access") =>
-  `${type}_token-${Math.random().toString(36).slice(2)}`;
-export const ACCESS_TTL = 50 * 1000; //5 detik
