49087838cfe89d752779534b0d164b6205a47c7e
restumardiko <mardikorestu@gmail.com>
2026-07-31 15:33:54 +0700

    update

--- src/domain/valueObjects/transactionType.js
+++ src/domain/valueObjects/transactionType.js
@@ -1,16 +1,45 @@
 export default class TransactionType {
-  static VALID_NAME = ["INCONE", "EXPENSE"];
-  #name;
-  #validCategories;
-  constructor({ name, validCategories }) {
-    this.#name = name; // "expense" or "income"
-    this.#validCategories = validCategories; // ["food", "health", ...]
+  static TYPE = { INCOME: "INCOME", EXPENSE: "EXPENSE" };
+  static typeIncome = TransactionType.TYPE.INCOME;
+  static typeExpense = TransactionType.TYPE.EXPENSE;
+  static CATEGORY = {
+    EXPENSE: ["FOOD", "TRANSPORT", "HOME", "HEALTH"],
+    INCOME: ["SALARY", "BUSINESS", "ETC."],
+  };
+
+  #type;
+  #validCategory;
+  constructor({ type, category }) {
+    this.#type = type;
+    this.#validCategory = category;
   }
 
-  static create({name,validCategories}) {
-    const normalizedName = name;
-    if (TransactionType.VALID_NAME.includes()) {
-      throw new Error("Transaction name is not valid");
+  static create({ type, category }) {
+    const normalizedType = type.toUpperCase();
+    const normalizedCategory = category.toUpperCase();
+    const whichType = TransactionType.TYPE[normalizedType];
+    const whichKindCategory = TransactionType.CATEGORY[normalizedType];
+
+    if (!whichType) {
+      throw new Error("neither Income nor Expense");
     }
+    if (!whichKindCategory.includes(normalizedCategory)) {
+      throw new Error("Category is not valid");
+    }
+
+    return new TransactionType({
+      type: normalizedType,
+      category: normalizedCategory,
+    });
+  }
+
+  get type() {
+    return this.#type;
+  }
+  get category() {
+    return this.#validCategory;
+  }
+  getTransactionType() {
+    return { type: this.#type, category: this.#validCategory };
   }
 }
---
+++ src/domain/valueObjects/transactionType.test.js
@@ -0,0 +1,25 @@
+import TransactionType from "./transactionType";
+
+test("reject invalid type", () => {
+  expect(() => {
+    TransactionType.create({ type: "notValid", category: "SALARY" });
+  }).toThrow(/^neither Income nor Expense$/);
+});
+test("reject invalid category on expense type", () => {
+  expect(() => {
+    TransactionType.create({ type: "expense", category: "SALARY" });
+  }).toThrow(/^Category is not valid$/);
+});
+test("reject invalid category on income type", () => {
+  expect(() => {
+    TransactionType.create({ type: "income", category: "FOOD" });
+  }).toThrow(/^Category is not valid$/);
+});
+test("return the validated transaction type and category", () => {
+  const base = TransactionType.create({ type: "Income", category: "Etc." });
+  console.log(base.getTransactionType());
+  expect(base.getTransactionType()).toEqual({
+    type: "INCOME",
+    category: "ETC.",
+  });
+});
